19b828ea0909a09e56ac42d63e4179c7df542ecd
Ryan Neil Stroud <ryanneilstroud@Ryans-MacBook-Pro.local>
2017-08-21 14:20:57 +0800

    Created App.js and Router.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './styles/ui.scss'
-import { MovieDayCount } from './components/MovieDayCount'
-import { MovieDayList } from './components/MovieDayList'
+import { App } from './components/App'
+import { NotFound404 } from './components/NotFound404'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
 render(
-  <MovieDayCount/>,
-  document.getElementById('react-container')
-)
-
-render(
-  <MovieDayList total={50}
-                  imax={20}
-                  threeD={10}
-                  goal={100}
-                  days={
-                  [
-                    {
-                      movie: 'Dunkirk',
-                      date: new Date('8/17/2017'),
-                      isImax: false,
-                      isThreeD: false
-                    },
-                    {
-                      movie: 'Valerian and the City of a Thousand Planets',
-                      date: new Date('8/4/2017'),
-                      isImax: false,
-                      isThreeD: false
-                    },
-                    {
-                      movie: 'Wonder Woman',
-                      date: new Date('7/5/2017'),
-                      isImax: false,
-                      isThreeD: false
-                    },
-                    {
-                      movie: 'Alien: Covenant',
-                      date: new Date('6/10/2017'),
-                      isImax: true,
-                      isThreeD: false
-                    }
-                  ]
-                  }/>,
+  <Router history={hashHistory}>
+    <Router path="/" component={App}></Router>
+    <Router path="*" component={NotFound404}></Router>
+  </Router>,
   document.getElementById('react-container')
 )
